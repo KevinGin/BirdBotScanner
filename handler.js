ce3e9293d99bcf985968dfa1fe8b55a0751c8904
Kevin Gin <kevingin@google.com>
2020-12-03 16:24:34 -0800

    switch request library and make async

--- handler.js
+++ handler.js
@@ -1,11 +1,17 @@
 "use strict";
 
+// todo -- clean up dependencies
 const request = require("request");
+const axios = require("axios");
 const AWS = require("aws-sdk");
 const twit = require("twit");
 const LOCATION = "US-CA-085"; // Santa Clara County, California, US
 
 module.exports.scan = async event => {
+  console.log("running scanner");
+  let status = 200;
+  let message = "**Your function executed successfully!";
+
   AWS.config.update({region: "us-west-2"});
   const s3 = new AWS.S3({ apiVersion: "2006-03-01" });
   const params = {
@@ -14,18 +20,27 @@ module.exports.scan = async event => {
 
   const speciesSeen = {};
 
-  function writeObservation(observation) {
+  async function writeObservation(observation) {
+    console.log('called write observation')
     const params = {
       "Bucket": LOCATION.toLowerCase(),
       "Key": observation.speciesCode,
       "Body": JSON.stringify(observation)
     };
-    s3.upload(params, function(err, data) {
-      if (err) throw new Error(err);
+    await s3.upload(params, function(err, data) {
+      if (err) {
+        console.log("failed to upload");
+        status = 500;
+        message = "failed to upload";
+        throw new Error(err);
+      } else {
+        console.log("WRITE SUCCESS");
+      }
     });
   }
 
-  function tweetObservation(observation) {
+  async function tweetObservation(observation) {
+    console.log('called tweet observation');
     let config = {
       consumer_key: process.env.CONSUMER_KEY,
       consumer_secret: process.env.CONSUMER_SECRET,
@@ -39,13 +54,17 @@ module.exports.scan = async event => {
     const commonName = observation.comName;
     const url = "https://ebird.org/checklist/" + observation.subId;
 
-    let tweet = commonName + " " + url;
+    let tweet = commonName + " " + Math.random().toPrecision(4);
 
-    Twitter.post("statuses/update", { status: tweet }, function(err, data, response) {
+    await Twitter.post("statuses/update", { status: tweet }, function(err, data, response) {
       if (err) {
+        console.log("failed to Tweet " + commonName);
         console.log(err);
+        status = 500;
+        message = "failed to tweet";
       } else {
         console.log("tweet posted for " + commonName);
+        message = "tweeted : " + commonName;
       }});
   }
 
@@ -61,7 +80,8 @@ module.exports.scan = async event => {
     "x00050",  // Herring x Glaucous-winged Gull (hybrid)
     "sxrgoo1", // Snow x Ross's Goose (hybrid)
     "houwre",  // House Wren
-    "yesfli",  // Northern Flicker (Yellow-shafted)
+    // commented out for development
+    // "yesfli",  // Northern Flicker (Yellow-shafted)
   ];
 
   const denyList = {};
@@ -70,55 +90,54 @@ module.exports.scan = async event => {
 
   // read S3 bucket for county, to compose set of species that have already been tweeted
   try {
+    console.log("making S3 read");
     const response = await s3.listObjects(params).promise();
     const content = response.Contents;
     content.forEach(obj => speciesSeen[obj.Key] = true);
   } catch (err) {
     console.log(err);
-    const message = `Error getting object ${key} from bucket ${bucket}. Make sure they exist and your bucket is in the same region as this function.`;
-    console.log(message);
+    status = 500;
+    message = "failed to read from S3";
     throw new Error(message);
   }
 
+  console.log("finished successfully reading S3");
 
-  // fetch notable observations from eBird
   var options = {
-    "method": "GET",
-    "url": "https://api.ebird.org/v2/data/obs/US-CA-085/recent/notable?detail=full",
-    "headers": {
+    method: "get",
+    url: "https://api.ebird.org/v2/data/obs/US-CA-085/recent/notable?detail=full",
+    headers: { 
       "X-eBirdApiToken": process.env.EBIRD_API_TOKEN
     }
   };
   
-  request(options, function (error, response) {
-    if (error) throw new Error(error);
-    const body = JSON.parse(response.body);
-
-    const observations = JSON.parse(response.body).filter(obs => !denyList[obs.speciesCode]);
-
-    //TODO(kevingin) handler should: write to S3 (and add to in-memory seen object)
-    // S3 write will then trigger lambda to tweet
-    observations.forEach(observation => {
+  await axios(options)
+    .then(async function (response) {
+      console.log("inside callback");
+      const data = response.data;
+      const observations = data.filter(obs => !denyList[obs.speciesCode]);
+      for (let i = 0; i < observations.length; i++) {
+      let observation = observations[i];
       const speciesCode = observation.speciesCode;
       if (!speciesSeen[speciesCode]) {
         speciesSeen[speciesCode] = true;
-        try {
-          // TODO(kevingin): move tweet action into separate lambda triggered by S3 bucket change
-          writeObservation(observation);
-          tweetObservation(observation);
-        } catch (err) {
-          console.log(err);
-        }
+        console.log("species: " + speciesCode);
+          await writeObservation(observation);
+          await tweetObservation(observation);
       }
+    }
+    })
+    .catch(function (error) {
+      console.log(error);
     });
-  });
+
 
   // TODO: Don't return 200 if caught exception
   return {
-    statusCode: 200,
+    statusCode: status,
     body: JSON.stringify(
       {
-        message: "Go Serverless v1.0! Your function executed successfully!",
+        message: message,
         input: event,
       },
       null,
