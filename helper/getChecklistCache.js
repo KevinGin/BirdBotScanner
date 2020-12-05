bf5bab4441aba1a2b59fd74798b26c47d1375008
Kevin Gin <kevingin@google.com>
2020-12-04 21:44:08 -0800

    modify eBird query to get only previous 2 days

--- helper/getChecklistCache.js
+++ helper/getChecklistCache.js
@@ -9,6 +9,7 @@ module.exports =  async function getChecklistCache(s3) {
     Bucket: CHECKLIST_CACHE_BUCKET,
   };
 
+  // (BUG) TODO: Create cache different way, returns max 1000 keys 
   const response = await s3.listObjects(params).promise();
   const content = response.Contents;
   content.forEach(obj => checklistCache[obj.Key] = true);
--- helper/getObservations.js
+++ helper/getObservations.js
@@ -3,10 +3,10 @@
 const axios = require("axios");
 
 module.exports =  async function getObservations() {
-  // all california observations US-CA
+  // all california observations US-CA in previous 2 days
   var options = {
     method: "get",
-    url: "https://api.ebird.org/v2/data/obs/US-CA/recent/notable?detail=full",
+    url: "https://api.ebird.org/v2/data/obs/US-CA/recent/notable?detail=full&back=2",
     headers: { 
       "X-eBirdApiToken": process.env.EBIRD_API_TOKEN
     }
